812715d3697f918459bcc3667b883e7707aa7f0b
qrizan <qrizan@gmail.com>
2024-02-18 00:20:34 +0700

    modify readme

--- src/utils/data.js
+++ src/utils/data.js
@@ -2,168 +2,168 @@ export const products = [
     {
         id:1,
         name: "( Dummy ) T-Shirts 1",
-        image: "../assets/products/t-shirt.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Tshirt&font=lobster",
         category: "T-Shirts",
         price: 13
     },
     {
         id:2,
         name: "( Dummy ) T-Shirts 2",
-        image: "../assets/products/t-shirt.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Tshirt&font=lobster",
         category: "T-Shirts",
         price: 14
     },
     {
         id:3,
         name: "( Dummy ) Pants 1",
-        image: "../assets/products/pants.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Pants&font=lobster",
         category: "Pants",
         price: 23
     },
     {
         id:4,
         name: "( Dummy ) T-Shirts 3",
-        image: "../assets/products/t-shirt.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Tshirt&font=lobster",
         category: "T-Shirts",
         price: 23
     },   
     {
         id:5,
         name: "( Dummy ) Shoes 1",
-        image: "../assets/products/shoes.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Shoes&font=lobster",
         category: "Shoes",
         price: 23
     },    
     {
         id:6,
         name: "( Dummy ) Shoes 2",
-        image: "../assets/products/shoes.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Shoes&font=lobster",
         category: "Shoes",
         price: 44
     },
     {
         id:7,
         name: "( Dummy ) Pants 2",
-        image: "../assets/products/pants.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Pants&font=lobster",
         category: "Pants",
         price: 44
     },
     {
         id:8,
         name: "( Dummy ) T-Shirts 4",
-        image: "../assets/products/t-shirt.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Tshirt&font=lobster",
         category: "T-Shirts",
         price: 11
     },
     {
         id:9,
         name: "( Dummy ) Shoes 3",
-        image: "../assets/products/shoes.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Shoes&font=lobster",
         category: "Shoes",
         price: 93
     },        
     {
         id:10,
         name: "( Dummy ) Pants 3",
-        image: "../assets/products/pants.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Pants&font=lobster",
         category: "Pants",
         price: 93
     },    
     {
         id:11,
         name: "( Dummy ) Shoes 4",
-        image: "../assets/products/shoes.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Shoes&font=lobster",
         category: "Shoes",
         price: 21
     },         
     {
         id:12,
         name: "( Dummy ) T-Shirts 5",
-        image: "../assets/products/t-shirt.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Tshirt&font=lobster",
         category: "T-Shirts",
         price: 43
     },
     {
         id:13,
         name: "( Dummy ) T-Shirts 6",
-        image: "../assets/products/t-shirt.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Tshirt&font=lobster",
         category: "T-Shirts",
         price: 21
     },
     {
         id:14,
         name: "( Dummy ) Shoes 5",
-        image: "../assets/products/shoes.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Shoes&font=lobster",
         category: "Shoes",
         price: 25
     },     
     {
         id:15,
         name: "( Dummy ) T-Shirts 7",
-        image: "../assets/products/t-shirt.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Tshirt&font=lobster",
         category: "T-Shirts",
         price: 63
     },    
     {
         id:16,
         name: "( Dummy ) T-Shirts 8",
-        image: "../assets/products/t-shirt.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Tshirt&font=lobster",
         category: "T-Shirts",
         price: 14
     },            
     {
         id:17,
         name: "( Dummy ) Shoes 6",
-        image: "../assets/products/shoes.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Shoes&font=lobster",
         category: "Shoes",
         price: 92
     },
     {
         id:18,
         name: "( Dummy ) Shoes 7",
-        image: "../assets/products/shoes.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Shoes&font=lobster",
         category: "Shoes",
         price: 21
     },    
     {
         id:19,
         name: "( Dummy ) Shoes 8",
-        image: "../assets/products/shoes.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Shoes&font=lobster",
         category: "Shoes",
         price: 43
     }, 
     {
         id:20,
         name: "( Dummy ) Pants 5",
-        image: "../assets/products/pants.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Pants&font=lobster",
         category: "Pants",
         price: 53
     },
     {
         id:21,
         name: "( Dummy ) Pants 6",
-        image: "../assets/products/pants.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Pants&font=lobster",
         category: "Pants",
         price: 92
     },                              
     {
         id:22,
         name: "( Dummy ) Pants 4",
-        image: "../assets/products/pants.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Pants&font=lobster",
         category: "Pants",
         price: 21
     },
     {
         id:23,
         name: "( Dummy ) Pants 7",
-        image: "../assets/products/pants.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Pants&font=lobster",
         category: "Pants",
         price: 21
     },    
     {
         id:24,
         name: "( Dummy ) Pants 8",
-        image: "../assets/products/pants.jpg",
+        image: "https://fakeimg.pl/300x300/?text=Pants&font=lobster",
         category: "Pants",
         price: 43
     },
